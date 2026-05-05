92abf72ed4958d3145e76e4ecb8c4d4b48d8125b
Jean Paul <jeanpaulortizrestrepo1@gmail.com>
2026-05-04 23:34:02 -0500

    Se organiza fusion del properties y rabbitConfig, el codigo presenta error por los 2 mapper de objetos para el broker

--- src/main/java/com/eatup/commercial/config/rabbitmq/CommercialRabbitMQConfig.java
+++ src/main/java/com/eatup/commercial/config/rabbitmq/CommercialRabbitMQConfig.java
@@ -73,18 +73,6 @@ public class CommercialRabbitMQConfig {
                 .to(commercialExchange)
                 .with(tableRoutingKey);
     }
-}
-package com.eatup.commercial.config.rabbitmq;
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CommercialRabbitMQConfig {
 
     @Bean
     public DirectExchange salesPatchRequestExchange(
